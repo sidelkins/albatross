98a082f03670c0f58a1efde276959e801d80c35e
Sid Elkins <sidelkins77@gmail.com>
2024-05-12 20:38:52 -0500

    fix imports

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { 
-    getUser, 
-    createUser, 
     getUserById, 
-    getUserByUsername } from '../controllers/userController.js';
+    getUserByUsername,
+    createUser
+} from '../controllers/userController.js';
 const router = express.Router()
 
 router.get('/get/byid', getUserById);
