5b998bd9e9717c813a73292f173a16ef5efeb631
Sid Elkins <sidelkins77@gmail.com>
2024-05-12 19:13:28 -0500

    switch to ESM; add middleware logging; add CORS; route organization

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import defaultRoutes from './routes/default.js'
+
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Enable CORS
+app.use(cors())
 
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${res.statusCode} ${req.method} ${req.url}`);
+  next();
+});
+
+// Routes
+app.use('/api', defaultRoutes)
+
+// Start Server
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
