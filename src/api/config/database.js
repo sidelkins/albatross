78e0f835923cc2a92e3c760527faff9e0d8389eb
sid elkins <sidelkins77@gmail.com>
2024-05-14 11:45:34 -0500

    use dotenv in database config; added example env file; started pg implementation

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -2,27 +2,57 @@ import Database from 'better-sqlite3';
 import knex from 'knex';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
+const validDatabases = ['better-sqlite3', 'pg']
 const dbType = process.env.DB_TYPE || 'better-sqlite3';
 const dbFile = process.env.DB_FILE || 'albatross.db';
+const dbHost = process.env.DB_HOST || 'localhost';
+const dbPort = process.env.DB_PORT;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const database = process.env.DATABASE;
+let config = {};
 
-// better-sqlite3 Database Creation
-if(dbType == 'better-sqlite3') {
-  try{ 
-    const db = new Database(dbFile);
-    db.pragma('journal_mode = WAL');
-    console.log(`[SUCCESS] create better-sqlite3 db`)
-  } catch (error) {
-    console.error(`[FAIL] create better-sqlite3 db: ${error}`)
+function createDatabase() {
+  try {
+    if(dbType == 'better-sqlite3') {
+      const db = new Database(dbFile);
+      db.pragma('journal_mode = WAL');
+
+      config = {
+        client: dbType,
+        connection: {
+          filename: dbFile
+        }
+      };
+    }
+    if(dbType == 'pg') {
+
+      config = {
+        client: dbType,
+        connection: {
+          host: dbHost,
+          port: dbPort,
+          user: dbUser,
+          password: dbPass,
+          database: database
+        }
+      }
+    }
+
+    if(!validDatabases.includes(dbType)) {
+      console.error(`[FAIL] '${dbType}' is not a valid database type`)
+    } else {
+      console.log(`[SUCCESS] create '${dbType}' database`)
+    }
+  } catch(error) {
+    console.error(`[FAIL] create ${dbType} db: ${error}`)
   }
+  
 }
 
-const config = {
-  client: dbType,
-  connection: {
-    filename: dbFile
-  }
-};
-
+createDatabase();
 const knexInstance = knex(config);
 
 export default knexInstance;
